3cef8ae4fe4c85183ecb677f1b4961b49968eef9
Yevhen <shershen.yevhen@gmai>
2023-12-03 09:54:28 +0100

    ES6 Math - nowe metody

---
+++ section6/script-part4.js
@@ -0,0 +1,30 @@
+/*
+
+LISTA INFORMACJI W TYM PLIKU (MOŻNA ODSZUKIWAĆ PO TYCH TREŚCIACH)
+
+143. ES6 Math - nowe metody
+*/
+
+//143. ES6 Math - nowe metody
+console.log('143. ES6 Math - nowe metody')
+
+console.log(Math.trunc(3.8))
+console.log(Math.trunc(7.2))
+
+console.log(Math.sign(3))
+console.log(Math.sign(0))
+console.log(Math.sign(-1))
+
+console.log(Math.cbrt(8))//2
+console.log(Math.cbrt(64))//4
+
+console.log(Math.log2(2))//1
+console.log(Math.log10(100))//2
+
+console.log(Number.isInteger(2))//true
+console.log(Number.isInteger(10.78))//false
+
+console.log(isFinite(10/1))//true
+console.log(isFinite(10/0))//false
+console.log(isNaN(10))//false
+console.log(isNaN(NaN))//true
