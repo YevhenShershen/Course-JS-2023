996d52bd4ef014984257680751c20e698af3fb37
Yevhen <shershen.yevhen@gmai>
2023-12-05 08:57:34 +0100

    151. ES6 Proxy api

--- section6/script-part6.js
+++ section6/script-part6.js
@@ -3,6 +3,7 @@
 LISTA INFORMACJI W TYM PLIKU (MOŻNA ODSZUKIWAĆ PO TYCH TREŚCIACH)
 
 150. ES6 Reflext api
+151. ES6 Proxy api
 */
 
 console.log("=======================SCRIPT-PART-6.JS===========================")
@@ -36,3 +37,47 @@ this.b = b
 }
 const obj = Reflect.construct(Test, [1,5])
 console.log(obj)//Test {a: 1, b: 5}
+
+
+
+//151. ES6 Proxy api
+console.log('151. ES6 Proxy api')
+
+let obj1 = {
+  a:10,
+  b: 99,
+  str: "text"
+}
+
+const handler = {
+  get(target, prop, receiver){
+    if(prop === "a") return "A!!!"
+    return target[prop] + "!";
+  },
+  set(target, prop, value){
+    if(prop === "a" || prop === "b") {
+      if(Number.isInteger(value)){
+        target[prop] = value
+      }
+    }else if(prop === "str" && typeof value === "string"){
+      target[prop] = value
+    }else{
+      console.log("WRONG DATA!")
+    }
+  }
+}
+
+const proxy1 = new Proxy(obj1,handler)
+console.log(proxy1.a)//A!!!
+console.log(proxy1.b)//99!!!
+console.log(Reflect.get(proxy1, "str"))//text!
+
+proxy1.b = 112
+console.log(proxy1.b)//112!
+proxy1.b = "test"
+console.log(proxy1.b)//112!
+
+proxy1.str = 23//WRONG DATA!
+console.log(proxy1.str)//text!
+proxy1.str = "OSA"
+console.log(proxy1.str)//OSA!
