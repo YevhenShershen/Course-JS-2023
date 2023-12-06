d3e4abe9e6f128e37dc350f7baaaa82dfcedf487
Yevhen <shershen.yevhen@gmai>
2023-12-06 11:43:17 +0100

    153. ES10 EcmaScript 2019

--- section6/script-part6.js
+++ section6/script-part6.js
@@ -4,6 +4,7 @@ LISTA INFORMACJI W TYM PLIKU (MOŻNA ODSZUKIWAĆ PO TYCH TREŚCIACH)
 
 150. ES6 Reflext api
 151. ES6 Proxy api
+153. ES10 EcmaScript 2019
 */
 
 console.log("=======================SCRIPT-PART-6.JS===========================")
@@ -81,3 +82,47 @@ proxy1.str = 23//WRONG DATA!
 console.log(proxy1.str)//text!
 proxy1.str = "OSA"
 console.log(proxy1.str)//OSA!
+
+
+///153. ES10 EcmaScript 2019
+console.log('153. ES10 EcmaScript 2019')
+
+const arr1 = [1,2,3,4,5, [,4,5,6,7,[33,44,55]]]
+//Metoda flat() spłaszcza zagnieżdżone w sobie tablice.
+// Możemy regulować poziom spłaszczenia podając do niej parametr depth .
+console.log(arr1.flat())// [1, 2, 3, 4, 5, 4, 5, 6, 7, Array(3)]
+console.log(arr1.flat(2))// [1, 2, 3, 4, 5, 4, 5, 6, 7, 33, 44, 55]
+
+console.log([1,3,4].flatMap( x=>[x, x*2]))//[1, 2, 3, 6, 4, 8]
+
+const entries = new Map([
+  ["a", 23],
+  ['str', 'text'],
+  ['obj', {data:10}]
+])
+
+const obj3 = Object.fromEntries(entries)
+console.log(obj3)//{a: 23, str: 'text', obj: {…}}
+
+console.log('    \r\n Hellow world   '.trimStart())//kasuje spacje z przodu
+console.log('     Hellow world \r\n   '.trimEnd())//kasuje spacje z końcu
+
+//pomijamy error tylko wżycamy informacje o Błędzie
+/*
+Było kiedyś w taki sposób
+try{
+  asd
+}catch(error){
+  console.log(error)
+}
+*/
+//teraz w ES10 (error) omijamy
+try{
+asd
+}catch{
+  console.log("Błąd")
+}
+
+const symbol = Symbol("Symbol info")
+console.log(symbol.toString(symbol))//Symbol(Symbol info)
+console.log(symbol.description)//Symbol info
