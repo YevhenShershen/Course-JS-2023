68b64f296726d3e67e4c940e96099b0ca316db9a
Yevhen <shershen.yevhen@gmai>
2023-12-07 08:17:36 +0100

    156. ES13 EcmaScript 2022

--- section6/script-part7.js
+++ section6/script-part7.js
@@ -3,6 +3,7 @@
 LISTA INFORMACJI W TYM PLIKU (MOŻNA ODSZUKIWAĆ PO TYCH TREŚCIACH)
 
 155. ES12 EcmaScript 2021
+156. ES13 EcmaScript 2022
 */
 
 console.log("=======================SCRIPT-PART-7.JS===========================")
@@ -53,3 +54,60 @@ let e = undefined; // null
 let f = 34;
 e ??= f
 console.log(e)//35
+
+
+
+//156. ES13 EcmaScript 2022
+console.log('156. ES13 EcmaScript 2022')
+class Car {
+  constructor(){
+    this.name = "Mustang";
+    this._brand = "Ford";
+    this.onClick = this.#clicked;
+  }
+
+    static PI = 3.14
+    static #TEXT = 'TXT'
+
+      //public instance field
+      color = "red";
+      topSpeed = 250;
+      currentOfficialPrice = 200_000;
+    //tworzymy prywatne pole (private instance field)
+      #currentPrice = 123_000;
+      printInfo =()=>{
+        console.log("currentOfficialPrice", this.currentOfficialPrice)
+        console.log("#currentPrice", this.#currentPrice)
+        console.log('#TEXT', Car.#TEXT)//TXT
+      };
+      //tworzymy prywatną funkcje
+      #clicked =()=>{
+        console.log('car clicked ' , this.name)
+      }
+      //statyczna metoda
+     static staticCallNewCarName =()=>{
+        console.log("new Car is ", this.name)
+      }
+      static getNewCar =()=>{
+        return new Car()
+      }
+}
+let car = new Car()
+console.log(car._brand)//Ford
+console.log(car.color)//red
+console.log(car.year)//undefined
+console.log(car.currentOfficialPrice)//200000
+// console.log(car.#currentPrice)//SyntaxError: Private field '#currentPrice' must be declared in an enclosing class
+car.printInfo()
+/*
+currentOfficialPrice 200000
+#currentPrice 123000
+*/
+car.onClick()//car clicked  Mustang
+setTimeout(car.onClick,2000)//car clicked  Mustang
+console.log(Car.PI)//3.14
+console.log(car.PI)//undefined
+console.log(Car.staticCallNewCarName())//new Car is  Car
+console.log(Car.getNewCar())//Car {color: 'red', topSpeed: 250, currentOfficialPrice: 200000, #currentPrice: 123000, #clicked: ƒ, …}
+
+//Top lvl await
