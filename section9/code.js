3354eda0f0b9b91e8482cb39d6ca2beca9a7b96e
Yevhen <shershen.yevhen@gmai>
2023-12-24 14:40:14 +0100

    161. Pobranie danych dzięki REST z serwera

--- section9/code.js
+++ section9/code.js
@@ -1,6 +1,7 @@
 let lat;
 let long;
-
+const apiKey = 'f581cefe7274768d9ea68a6290d7776e'
+const unit = 'metric'
 
 function startApp(){
   if(navigator.geolocation){
@@ -10,8 +11,21 @@ function startApp(){
       long = position.coords.longitude;
 
       console.log(`lat: ${lat}, long: ${long}`);
+
+      getWeatherData()
     }
     )
   }
 }
 
+async function  getWeatherData(){
+  let url= `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=${unit}&appid=${apiKey}`;
+try{
+  const response = await fetch(url)
+  const data = await response.json();
+  console.log(data)
+}
+catch (err){
+console.log(err)
+}
+}
