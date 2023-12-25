c1a4e1a7bf4c5425cec77907fa42ea04359ec69d
Yevhen <shershen.yevhen@gmai>
2023-12-25 10:56:26 +0100

    162. Uzupełnienie html danymi z serwera

--- section9/code.js
+++ section9/code.js
@@ -24,8 +24,42 @@ try{
   const response = await fetch(url)
   const data = await response.json();
   console.log(data)
+  updateWeatherData(data)
 }
 catch (err){
 console.log(err)
 }
+}
+
+
+function updateWeatherData(data){
+const {current} = data
+
+const temp = current.temp;
+const pressure = current.pressure
+const humidity = current.humidity
+const clouds = current.clouds
+const windSpeed = current.wind_speed
+const sunRise = new Date(current.sunrise * 1000)
+const sunSet = new Date (current.sunset * 1000)
+
+document.getElementById("temp").innerHTML = temp
+document.getElementById("pressure").innerHTML = pressure
+document.getElementById("humidity").innerHTML = humidity
+document.getElementById("cloudsPerc").innerHTML = clouds
+document.getElementById("windSpeed").innerHTML = windSpeed
+document.getElementById("sunRise").innerHTML = sunRise.getHours() + " : " + sunRise.getMinutes()
+document.getElementById("sunSet").innerHTML = sunSet.getHours() + " : " + sunSet.getMinutes()
+
+
+let imgIcon = current.weather[0].icon
+
+let imgUrl = `https://openweathermap.org/img/wn/${imgIcon}@2x.png`
+document.getElementById('currentWeatherImg').setAttribute("src", imgUrl);
+
+const timeZone = data.timezone
+const locationLink = document.getElementById('locationLink')
+locationLink.innerHTML = timeZone
+
+locationLink.href = `https://openstreetmap.org/#map=20/${lat}/${long}`
 }
