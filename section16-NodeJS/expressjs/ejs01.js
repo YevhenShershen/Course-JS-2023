ec9333ea191c67e7e57b33fbfc373381918aa98c
Yevhen <shershen.yevhen@gmai>
2024-02-14 19:50:59 +0100

    208. ExpressJs: szablony html w EJS

---
+++ section16-NodeJS/expressjs/ejs01.js
@@ -0,0 +1,27 @@
+//npm install ejs
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const linksData = [
+ {name: "Home" , url: "/"},
+ {name: "Artykuły", url: "/articles"},
+{name: "Google", url:"https://google.com"}
+]
+
+const viewsPath = path.join(__dirname, 'views');
+app.set("views", viewsPath);
+app.set('view engine', "ejs");
+app.use(express.static("./public"))
+
+app.get("/", (req, res)=> {
+  res.render("pages/home",{
+    pageHeading: "Strona www",
+    pageTitle: "Strona główna",
+    links: linksData
+  })
+
+})
+
+app.listen(8080,()=>{console.log("Server started")})
