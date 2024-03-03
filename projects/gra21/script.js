6c3b1208b0d91f17c18101c124882be2e23e7a69
Yevhen <shershen.yevhen@gmai>
2024-03-03 20:49:14 +0100

    My project part 2 (game 21)

--- projects/gra21/script.js
+++ projects/gra21/script.js
@@ -5,20 +5,48 @@ window.onload = function (){
 //buttons
 const playWithComputer = document.getElementById('playWithComputer')
 const restartGame = document.getElementById('restartGame')
+const hitButton = document.getElementById('hitButton')
+const stopButton = document.getElementById('stopButton')
+const nextGameButton = document.getElementById('nextGameButton')
 
-const leftCardText = document.getElementById('left-card-text')
-const cardPoits = document.getElementById('cardPoits')
+
+//===============================
+const leftCardText = document.getElementById('leftCardText')
+const rightCardText = document.getElementById('rightCardText')
+const leftCardPoints = document.getElementById('leftCardPoints')
+const rightCardPoints = document.getElementById('rightCardPoints')
+const resultInfo = document.getElementById('resultInfo')
 const randomIds = []
-let randomCard;
+let randomCards = [];
 let points = 0;
-
+let userResul = 0;
+let computerResult = 0;
 class App {
   init(){
-  console.log("START App", store.giveCard())
-  buttonMenu.startGameWithComputer()
+  console.log("START App")
+
   restartGame.addEventListener('click', ()=>{
     buttonMenu.restartGame()
   })
+
+  hitButton.addEventListener('click', ()=>{
+    buttonMenu.startGameWithComputer()
+  })
+
+  stopButton.addEventListener('click', ()=>{
+    buttonMenu.stopUserButton()
+  })
+
+  nextGameButton.addEventListener('click',()=>{
+    buttonMenu.resetGame()
+  } )
+
+  playWithComputer.addEventListener('click', ()=>{
+    buttonMenu.startGame()
+  })
+
+  ui.disabledButton()
+  ui.disabledNextGameButton()
   }
 }
 
@@ -99,36 +127,107 @@ class Store {
       randomId = Math.floor(Math.random() * 52)
     }while(randomIds.includes(randomId))
     randomIds.push(randomId)
-    randomCard = this.addIdToElement()[randomId]
+    randomCards.push(this.addIdToElement()[randomId])
+    }
+
+    resetRandomCards = function (){
+      randomCards = []
     }
   }
 
 
 class Ui{
   showCardDeal = function(){
-    console.log('TEST',randomCard.name)
-    leftCardText.innerHTML = `${randomCard.name}`
-  }
-  showPoints = function(){
-    console.log(points , randomCard.value)
-    cardPoits.innerHTML = `POINTS: ${points + randomCard.value}`
+    leftCardText.innerHTML = randomCards.map(el => `${el.name}`)
   }
 
+  showOverPoints = function(points){
+    if(points > 21){
+      this.disabledButton()
+      userResul++
+      this.setCardPointsStyle()
+      this.showResultInfo()
+      this.enebledNextGameButton()
+    }
+  }
+
+  showPoints = function(elem){
+    points = randomCards.map(el =>el.value).reduce((accumulator, currentValue) => accumulator + currentValue)
+    leftCardPoints.innerHTML = `POINTS: ${points}`
+
+    this.showOverPoints(points)
+  }
+
+  showResultInfo = function (){
+    resultInfo.innerHTML = `RESULT: ${userResul} | ${computerResult}`
+  }
+
+  disabledButton= function(){
+    hitButton.classList.add('disabled')
+    stopButton.classList.add('disabled')
+  }
+
+  enabledButton= function(){
+    hitButton.classList.remove('disabled')
+    stopButton.classList.remove('disabled')
+  }
+
+  setCardPointsStyle= function(){
+    leftCardPoints.classList.add('text-danger')
+    if(!randomCards.length) leftCardPoints.classList.remove('text-danger')
+  }
+
+  resetCardContent = function (){
+    leftCardPoints.innerHTML = `POINTS:`
+    leftCardText.innerHTML = ''
+    rightCardPoints.innerHTML = `POINTS:`
+    rightCardText.innerHTML = ''
+  }
+
+  disabledNextGameButton = function (){
+    nextGameButton.classList.add('disabled')
+  }
+  enebledNextGameButton = function (){
+    nextGameButton.classList.remove('disabled')
+  }
 }
 
 
 
 class ButtonMenu {
   startGameWithComputer = function(){
-      playWithComputer.addEventListener('click', ()=>{
       playWithComputer.classList.add('disabled')
       store.giveCard()
       ui.showCardDeal()
       ui.showPoints()
-      })
   }
+
   restartGame = function(){
     playWithComputer.classList.remove('disabled')
+  }
+
+
+  stopUserButton = function (){
+    ui.disabledButton()
+    store.resetRandomCards()
+  }
+
+
+  resetGame = function(){
+    store.resetRandomCards()
+    ui.resetCardContent()
+    ui.enabledButton()
+    ui.setCardPointsStyle()
+    this.startGame()
+  }
+
+
+  startGame = function (){
+    buttonMenu.startGameWithComputer()
+    setTimeout(()=>{
+      buttonMenu.startGameWithComputer()
+      ui.enabledButton()
+    },1000)
   }
 }
 
