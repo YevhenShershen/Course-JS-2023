2a35666de659bf9534774d24d25c0257b728e5ec
Yevhen <shershen.yevhen@gmai>
2024-05-25 10:48:55 +0200

    243. MongoDB - instalacja na windows/ 244. MongoDB - listowanie dostępnych baz/245. MongoDB - listowanie kolekcji w bazie local czyli dostępnych pseudo tabel

---
+++ section25-DB/mongodb/list_collections.js
@@ -0,0 +1,30 @@
+const mongo = require("mongodb")
+const MongoClient = mongo.MongoClient;
+
+async function processDB(){
+  const url = "mongodb://127.0.0.1:27017";
+  const client = new MongoClient(url, { monitorCommands: false });
+
+  client.on("commandStarted", data => console.log("commandStarted:", data))
+
+  client.on("commandSucceeded", data => console.log("commandSucceeded:", data))
+
+  client.on("commandFailed", data => console.log("commandFailed:", data))
+
+  try{
+    await client.connect();
+
+    const db = client.db("local");
+
+    const collections = await db.listCollections().toArray();
+    console.log(collections)
+  }catch(err){
+    console.log(err)
+  }
+  finally{
+    await client.close()
+  }
+
+}
+
+processDB()
